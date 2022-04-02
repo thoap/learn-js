8fe98f208f3b469e5c716ed61895057ba6960095
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-02 15:55:17 +0200

    Zeigt eine weitere Art das Datum einzugeben

--- dates_and_times.js
+++ dates_and_times.js
@@ -1,7 +1,8 @@
 let val;
 
 const today = new Date();
-const birthday = new Date('1981-09-10 11:25:00');
+let birthday = new Date('1981-09-10 11:25:00');
+birthday = new Date('September 10 1981');
 
 val = birthday.toString();
 
