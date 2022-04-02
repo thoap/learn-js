073ef690a54069e1b70325800effcff08390bda5
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-02 15:30:49 +0200

    Initialer Commit

---
+++ object_literals.js
@@ -0,0 +1,26 @@
+const person = {
+    firstName: 'Steve',
+    lastName: 'Smith',
+    age: 36,
+    email: 'steve@aol.com',
+    hobbies: ['music', 'sports'],
+    address: {
+        city: 'Miami',
+        state: 'FL'
+    },
+    getBirthYear: function () {
+        return 1986;
+    }
+}
+
+let val;
+
+val = person;
+
+val = person.firstName;
+val = person.hobbies[1];
+val = person.getBirthYear();
+
+console.log(val);
+
+
