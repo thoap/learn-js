a00c3c80f1f549d7b1782699b2e3da54673042a6
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-02 15:58:51 +0200

    Zeigt wie man den numerischen WochenTag des Datums extrahiert

--- dates_and_times.js
+++ dates_and_times.js
@@ -4,6 +4,8 @@ const today = new Date();
 let birthday = new Date('1981-09-10 11:25:00');
 birthday = new Date('September 10 1981');
 
+val = today.getMonth();
 val = today.getDate();
+val = today.getDay();
 
 console.log(val);
