b18f4f46b304eb046706c04175337c6130d347f9
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-02 15:35:05 +0200

    Schreibt einen kleinen Loop

--- object_literals.js
+++ object_literals.js
@@ -23,4 +23,12 @@ val = person.getBirthYear();
 
 console.log(val);
 
+const people = [
+    {name: 'John', age: 30},
+    {name: 'Mike', age: 23},
+    {name: 'Nancy', age: 23}
+]
 
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].name);
+}
