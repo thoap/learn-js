be4afd2ebe14dc5fa83223017172c9915874ee0d
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-02 15:57:44 +0200

    Zeigt wie man den Tag des Datums extrahiert

--- dates_and_times.js
+++ dates_and_times.js
@@ -4,6 +4,6 @@ const today = new Date();
 let birthday = new Date('1981-09-10 11:25:00');
 birthday = new Date('September 10 1981');
 
-val = today.getMonth();
+val = today.getDate();
 
 console.log(val);
