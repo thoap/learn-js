50f5dc038d4099a66b5385243c1ecf54655bb0a8
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-08 17:43:45 +0200

    Zeigt die Benutzung von `continue` im Loop

--- loops_and_iterations.js
+++ loops_and_iterations.js
@@ -2,9 +2,11 @@
 
 for(let i = 0; i < 10; i++) {
 
-    console.log('Number ' + i);
-
     if (i === 2) {
         console.log('2 is my favorite Number');
+        continue;
     }
+
+    console.log('Number ' + i);
+
 }
