ff600d991c75d0843353172b34c2d899c6f5fc59
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-08 13:36:01 +0200

    Zeigt, wie man einen swich benutzt

---
+++ switches.js
@@ -0,0 +1,14 @@
+const color = 'red';
+
+switch (color) {
+    case 'red':
+        console.log('RED!');
+        break;
+    case 'blue':
+        console.log('BLUE');
+        break;
+    default:
+        // statements_def
+        console.log('Color is neither red nor blue!')
+        break;
+}
