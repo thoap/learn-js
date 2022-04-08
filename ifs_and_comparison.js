9143e54461faaa539788860c1cd0286009440533
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-08 13:17:59 +0200

    Wirft einen Fehler, wenn man testen will, ob eine Variable existiert

--- ifs_and_comparison.js
+++ ifs_and_comparison.js
@@ -1,24 +1,24 @@
-const id = 100;
+// const id = 100;
 
-if (id == 100) {
-    console.log('CORRECT!');
-    console.log('Be aware, input is a string, but test is on an integer.');
-} else {
-    console.log('INCORRECT!')
-}
+// if (id == 100) {
+//     console.log('CORRECT!');
+//     console.log('Be aware, input is a string, but test is on an integer.');
+// } else {
+//     console.log('INCORRECT!')
+// }
 
-if (id != 100) {
-    console.log('CORRECT!');
-} else {
-    console.log('INCORRECT!');
-}
+// if (id != 100) {
+//     console.log('CORRECT!');
+// } else {
+//     console.log('INCORRECT!');
+// }
 
-// test value and type at the same Time
-if (id === 100) {
-    console.log('CORRECT!');
-} else {
-    console.log('INCORRECT! Given id is different from int 100.');
-}
+// // test value and type at the same Time
+// if (id === 100) {
+//     console.log('CORRECT!');
+// } else {
+//     console.log('INCORRECT! Given id is different from int 100.');
+// }
 
 
 if (id) {
