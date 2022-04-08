08e993b7937dbd2b7fea946d90026db2a70149fd
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-08 17:43:10 +0200

    Zeigt einen Loop mit eingebetteter if-Bedingung

--- loops_and_iterations.js
+++ loops_and_iterations.js
@@ -1,5 +1,10 @@
 // For Loop
 
 for(let i = 0; i < 10; i++) {
-    console.log('Number ' + i)
+
+    console.log('Number ' + i);
+
+    if (i === 2) {
+        console.log('2 is my favorite Number');
+    }
 }
