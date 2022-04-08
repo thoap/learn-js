b7fe9f513cac9d6cb8e3af039da56bc3f88d10d5
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-08 13:40:00 +0200

    Zeigt einen switch mit vielen Optionen

--- switches.js
+++ switches.js
@@ -12,3 +12,34 @@ switch (color) {
         console.log('Color is neither red nor blue!')
         break;
 }
+
+
+let day;
+switch (new Date().getDay()) {
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+    default:
+        // statements_def
+        break;
+}
+
+console.log(`Today is ${day}.`);
