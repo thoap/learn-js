0dd66334daa2d34218c78670be5fc54d4eba4095
Thomas Apolke <thomas.apolke@berlin-recycling.de>
2022-04-08 12:57:58 +0200

    Zeigt, wie trügerisch ein doppeltes Gleichheitszeichen sein kann
    Auch ein Vergleich eines Strings mit einem Integer wird als korrekt
    zurückgegeben.

--- ifs_and_comparison.js
+++ ifs_and_comparison.js
@@ -1,7 +1,16 @@
-const id = 100;
+const id = '100';
 
-if (id == 110) {
+if (id == 100) {
+    console.log('CORRECT!');
+    console.log('Be aware, input is a string, but test is on an integer.');
+} else {
+    console.log('INCORRECT!')
+}
+
+if (id != 100) {
     console.log('CORRECT!');
 } else {
     console.log('INCORRECT!')
 }
+
+// test value and type at the same Time
